dbc04911deb4dcfbda6d3d5773023400e94f865a
Twinkle <1027293872@qq.com>
2020-10-31 21:52:59 +0800

    update com.twinkle.utils

---
+++ src/main/java/com/twinkle/utils/PasswordEncodeUtils.java
@@ -0,0 +1,28 @@
+package com.twinkle.utils;
+
+public class PasswordEncodeUtils {
+    private final static String[] hexDigits = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+
+    public static String generatePassword(String inputString) {
+        return encodeByMD5(inputString);
+    }
+
+    private static String encodeByMD5(String originString) {
+        if (originString != null)
+    }
+
+    private static String byteArrayToHexString(byte[] b) {
+        StringBuffer resultSb = new StringBuffer();
+        for (int i = 0; i < b.length; i++) {
+            resultSb.append(byteToHexString(b[i]));
+        }
+    }
+
+    private static String byteToHexString(byte b) {
+        int n = b;
+        if (n < 0) n = 256 + n;
+        int d1 = n / 16;
+        int d2 = n % 16;
+        return hexDigits[d1] + hexDigits[d2];
+    }
+}
